142653629a45053f3d0072ee337a85cc1a5df104
Alien Goo <glenn.android@gmail.com>
2015-09-24 23:20:54 +0100

    wip

--- Leads/src/StartUp/WebApiRegistration.cs
+++ Leads/src/StartUp/WebApiRegistration.cs
@@ -1,5 +1,7 @@
 ﻿using System.Web.Http;
 using Leads.Configuration;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
 using Owin;
 
 namespace Leads.StartUp
@@ -13,6 +15,21 @@ namespace Leads.StartUp
             config.MapHttpAttributeRoutes(new CustomDirectRouteProvider());
 
             app.UseWebApi(config);
+
+            var physicalFileSystem = new PhysicalFileSystem(@"./wwwroot");
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[]
+            {
+                "index.html"
+            };
+
+            app.UseFileServer(options);
         }
     }
 }
